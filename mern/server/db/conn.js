8390a6da1ff9ad4a1c57c40a73034ee01fc7cb38
rudolfKischer <rudikischer@gmail.com>
2022-11-19 17:21:45 -0500

    init-commit-example-client-server

---
+++ mern/server/db/conn.js
@@ -0,0 +1,28 @@
+const { mongoClient, MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+    useNewUrlParser:true,
+    // useUnifedTopology: true,
+});
+
+var _db;
+
+module.exports = {
+
+    connectToServer: function (callback) {
+        client.connect(function (err, db) {
+            // verify we got a good db object
+                if (db)
+                {
+                    _db = db.db("employees");
+                    console.log("Successfully connected to MongoDB.");
+                }
+                return callback(err);
+            });
+    },
+
+    getDb: function () {
+        return _db;
+    },
+    
+};
